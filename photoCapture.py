ab8a54d79ca1328bf39fe86fa6992964560397f0
achadha0111 <achadharocks@gmail.com>
2017-10-14 23:34:09 +0200

    Adding photo capture flask script

---
+++ photoCapture.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, request, Response, jsonify
+import json
+import nexmo
+import sys
+import requests
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def triggerPhotoCapture():
+    ''' Insert your photo capture logic here '''
+
+
+
+    ''' Once photos have been captured, call Esraa's function to post to Twitter '''
+    return Response(), 200
+
+def postToTwiiter():
+    ''' Fill in this function with logic to post to twitter '''
+    return True 
+if __name__ == "__main__":
+    app.run(debug=True)
